cb7e8792862b11574c2592a0aa12854a78c5ddcb
Martin Radicke (delmarad) <martin.radicke@gmail.com>
2023-06-18 23:20:51 +0200

    added illustrations to the Data Preproc. chapter

--- pages/3_⚙️_Data Preprocessing.py
+++ pages/3_⚙️_Data Preprocessing.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Data Preprocessing")
 
@@ -16,6 +17,14 @@ allowing us to work with larger batches of images during model training. The dat
  to 224 x 224 x 3. The model we chose for transfer learning, ResNet-50v2, is specifically designed to work with
  images of this particular size.
 
+""")
+
+st.image(Image.open('images/preproc_resizing.png'))
+st.image(Image.open('images/preproc_normalizing.png'), caption="Standard Data Preprocessing Pipeline")
+st.image(Image.open('images/preproc_resnet_resizing.png'), caption="Data Preprocessing for the ResNet-50v2 model")
+
+st.markdown("""
+
 Another aspect of data preprocessing is data normalization. Normalization transforms the pixel values of the 
 images to a standardized range, typically between 0 and 1. This process helps to mitigate the effects of varying
  pixel intensity distributions across different images. Normalization is achieved by dividing each pixel value 
@@ -27,11 +36,25 @@ Additionally, we use LabelEncoder, from scikit-learn library, to facilitate the 
 variables into numerical representations. This function converts each unique category into a unique integer, 
 enabling machine learning algorithms to process the data effectively.
 
+""")
+
+st.image(Image.open('images/preproc_encoding.png'), caption="Label encoding for three different classification tasks")           
+
+st.markdown("""
+
+
+
 For the plant categorization task, to address the issue of class imbalance, we use RandomUnderSampler, from the 
 imblearn library. It is a form of undersampling, which aims to reduce the number of samples from the majority 
 class to balance it with the minority class. By removing instances from the majority class, it helps prevent 
 the model from being biased towards the majority class during training and allows it to give equal importance to 
 both classes. 
+
+""")
+
+st.image(Image.open('images/Preproc_randomundersampler.png'), caption="Random Undersampling")
+
+st.markdown("""
 
 Finally, for the 2 most complex tasks, plant categorization and plant-disease classification, we apply data 
 further augmentation techniques to the training dataset, through the use of ImageDataGenerator, a preprocessing 
